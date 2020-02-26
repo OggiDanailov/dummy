39de60a74295436cd3590b340aced18d73224445
OggiDanailov <ogidan@abv.bg>
2020-02-25 19:22:15 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// 1. Write a function that takes two 
+// integers as parameters, adds them 
+// together and checks if the result is 
+// even or odd number.
+
+// function adding (x, y) {
+// 	var result = x+y;
+// 	if(result % 2 === 0) {
+// 		console.log('the sum is even')
+// 	} else {
+// 		console.log("the sume is odd")
+// 	} 
+// }
+
+// 2. Write a function that takes a word
+ // (string) as parameter and checks
+ //  if this word is a palindrome;
+ //   For example -> kayak, racecar
+ //    are palindromes;
+// 99.9% of the English words are not;
+
+// function pali (word) {
+// 	var backwards = word.split(" ")
+// 	var backwards2 = backwards.reverse()
+// 	var backwards3 = backwards2.join("")
+// 	if(word === backwards3) {
+// 		console.log(backwards3 + " is a palindrome")
+// 	} else {
+// 		console.log(backwards3 + " is not a palindrome")
+// 	}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
